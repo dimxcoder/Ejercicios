6bc71ca1697e9fb11a72838c5ebf5b935cc49f29
dimxcoder <134644134+dimxcoder@users.noreply.github.com>
2023-06-08 20:00:40 +0200

    Create Class-1.py

---
+++ Class-1.py
@@ -0,0 +1,10 @@
+num=int(input("Introduzca un numero: ")) #6
+
+while num!=1:
+    if num%2==0:
+        num = num / 2
+        print(num)
+    elif num%2!=0 :
+        num=(num*3)+1
+        print(num)
+
