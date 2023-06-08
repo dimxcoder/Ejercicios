e3e26cf93fdd10420397e104f2c842676eccbda0
dimxcoder <134644134+dimxcoder@users.noreply.github.com>
2023-06-08 20:55:17 +0200

    a

--- Class-2.py
+++ Class-2.py
@@ -1,9 +1,8 @@
-num=int(input("Enter a number: "))
-x=0
-y=1
-for i in range(1, 11):
-    while y<=10:
-        x=num*y
-        y+=1
-        
-print(x)
+print("--------------TABLA DE MULTIPLICAR------------")
+
+mult=int(input("Introduzca un número:"))
+imprimir=""
+for i in range(1,11):
+    imprimir +=  str(mult*i)
+
+print(f"Resultado Tabla {imprimir} ")
